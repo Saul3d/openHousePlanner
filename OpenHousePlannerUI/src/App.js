ea588ff78bb8ecef99c0a3990611626fac82406e
saul3d <saul3d@yahoo.com>
2020-02-03 22:20:41 -0600

    Added landingPage, Header, and Footer component

--- OpenHousePlannerUI/src/App.js
+++ OpenHousePlannerUI/src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import LandingPage from './components/LandingPage/LandingPage';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>   
-    </div>
+    <React.Fragment>
+      <Header />
+      <LandingPage />
+      <Footer />
+    </React.Fragment>
   );
 }
 
